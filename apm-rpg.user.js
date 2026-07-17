f6287bec6289a3b84c117aba938499cead164e85
Josiah Bailey <josiahbailey101@yahoo.com>
2026-07-17 12:59:17 -0400

    Update testing.

--- apm-rpg.user.js
+++ apm-rpg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         APM RPG
 // @namespace    https://w.amazon.com/bin/view/Users/baijosis/APM-RPG/
-// @version      1.0.9
+// @version      1.0.10
 // @description  Gamified RPG layer over APM/PTP - levels, EXP, roaming pets, wild pet catching.
 // @author       baijosis
 // @match        https://*.eam.hxgnsmartcloud.com/*
